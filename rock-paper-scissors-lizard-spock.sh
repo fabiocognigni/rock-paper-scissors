7a07732ce37526aec9df1b02f832fa81ebba8014
Fabio Cognigni <fabio.cognigni@gmail.com>
2015-10-19 09:12:10 +0100

    Added convenient play game scripts

---
+++ rock-paper-scissors-lizard-spock.sh
@@ -0,0 +1,8 @@
+jarFile=target/scala-2.11/rock-paper-scissors-1.0.jar
+
+if ! [ -a $jarFile ]  ; then
+	echo "Executable JAR not found: building from sources ..."
+	sbt assembly
+fi
+
+java -Dgame=extended -jar $jarFile $@
